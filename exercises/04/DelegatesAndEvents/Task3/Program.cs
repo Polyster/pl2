ffb03f668594b490c0640c57a65a1517af5d21a1
Maroš Pohančeník <maros.pohancenik@medelogics.onmicrosoft.com>
2019-03-13 17:28:24 +0100

    Commented stuff out

--- exercises/04/DelegatesAndEvents/Task3/Program.cs
+++ exercises/04/DelegatesAndEvents/Task3/Program.cs
@@ -28,20 +28,20 @@ namespace Task3
 
 			// TODO: Implement Benchmark method
 			// Using lambda
-			avg = ActionBenchmarker.Benchmark(5, () => Thread.Sleep(20));
-			Console.WriteLine($"Average time was: {avg}");
+			//avg = ActionBenchmarker.Benchmark(5, () => Thread.Sleep(20));
+			//Console.WriteLine($"Average time was: {avg}");
 
-			// Using static method
-			avg = ActionBenchmarker.Benchmark(5, () => ExamplesFactory.GetTask());
-			Console.WriteLine($"Average time was: {avg}");
+			//// Using static method
+			//avg = ActionBenchmarker.Benchmark(5, () => ExamplesFactory.GetTask());
+			//Console.WriteLine($"Average time was: {avg}");
 
-			// Using lambda
-			avg = ActionBenchmarker.Benchmark(5, i => Thread.Sleep(i));
-			Console.WriteLine($"Average time was: {avg}");
+			//// Using lambda
+			//avg = ActionBenchmarker.Benchmark(5, i => Thread.Sleep(i));
+			//Console.WriteLine($"Average time was: {avg}");
 
-			// Using static method
-			avg = ActionBenchmarker.Benchmark(5, i => ExamplesFactory.GetTask(i));
-			Console.WriteLine($"Average time was: {avg}");
+			//// Using static method
+			//avg = ActionBenchmarker.Benchmark(5, i => ExamplesFactory.GetTask(i));
+			//Console.WriteLine($"Average time was: {avg}");
 
 			#endregion
 
@@ -49,8 +49,8 @@ namespace Task3
 
 			// TODO: Implement Operate method
 			var operation = new BinaryOperation<int>(5, 5);
-			operation.Operate((a, b) => a + b);
-			operation.Operate((a, b) => a - b);
+			//operation.Operate((a, b) => a + b);
+			//operation.Operate((a, b) => a - b);
 			// TODO: Add multiplication and division calls
 
 			#endregion
